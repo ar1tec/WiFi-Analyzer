4dd664b1d7eaf20336eb4aead6924280c1e7e1ea
Pravin Kadam <31037035+galaxydevelopers@users.noreply.github.com>
2017-10-28 00:13:20 +0530

    Activity Added

---
+++ app/src/main/java/com/galaxy/wifianalyzar/about/AdvertisementActivity.java
@@ -0,0 +1,171 @@
+/*
+ * WiFiAnalyzer Copyright (C) 2017 VREM Software Development <VREMSoftwareDevelopment@gmail.com>"
+ *
+ * License:WiFi Analyzer is licensed under the GNU General Public License v3.0 (GNU GPLv3)
+ * License Details at "http://www.gnu.org/licenses/gpl-3.0.html"
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ *
+ * Get Full Source Code"https://github.com/galaxydevelopers/WiFi-Analyzer"
+ */
+package com.galaxy.wifianalyzar.about;
+
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.facebook.ads.Ad;
+import com.facebook.ads.AdChoicesView;
+import com.facebook.ads.AdError;
+import com.facebook.ads.AdListener;
+import com.facebook.ads.AdSettings;
+import com.facebook.ads.MediaView;
+import com.facebook.ads.NativeAd;
+import com.galaxy.wifianalyzar.MainContext;
+import com.galaxy.wifianalyzar.R;
+import com.galaxy.wifianalyzar.settings.Settings;
+import com.galaxy.wifianalyzar.settings.ThemeStyle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdvertisementActivity extends AppCompatActivity  {
+
+    private NativeAd nativeAd;
+    private LinearLayout  nativeAdContainer;
+    private LinearLayout adView;
+    private ProgressBar spinner;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Settings settings = MainContext.INSTANCE.getSettings();
+        ThemeStyle themeStyle = settings.getThemeStyle();
+        setTheme(themeStyle.themeAppCompatStyle());
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advertisement);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+
+
+
+        spinner = (ProgressBar)findViewById(R.id.progressBar1);
+
+
+        //Gads.setVisibility(View.GONE);
+        spinner.setVisibility(View.VISIBLE);
+
+
+
+showNativeAd();
+
+
+}
+
+
+
+
+    private void showNativeAd() {
+        nativeAd = new NativeAd(this, "1755828284679452_1756620677933546");
+
+        AdSettings.addTestDevice("15eac3c9005ef5163760c47d01b2b94a");
+       // AdSettings.addTestDevice("d1f8d5642535f38480ef36c5f80356c8");
+        nativeAd.setAdListener(new AdListener() {
+
+            @Override
+            public void onError(Ad ad, AdError error) {
+                spinner.setVisibility(View.GONE);
+                TextView textview=(TextView)findViewById(R.id.ads_textview);
+                textview.setVisibility(View.VISIBLE);
+                // Ad error callback
+            }
+
+            @Override
+            public void onAdLoaded(Ad ad) {
+                spinner.setVisibility(View.GONE);
+                if (nativeAd != null) {
+                nativeAd.unregisterView();
+            }
+
+                // Add the Ad view into the ad container.
+                nativeAdContainer = (LinearLayout) findViewById(R.id.native_ad_container);
+                LayoutInflater inflater = LayoutInflater.from(AdvertisementActivity.this);
+                // Inflate the Ad view.  The layout referenced should be the one you created in the last step.
+                adView = (LinearLayout) inflater.inflate(R.layout.native_ad_layout, nativeAdContainer, false);
+                nativeAdContainer.addView(adView);
+
+                // Create native UI using the ad metadata.
+                ImageView nativeAdIcon = (ImageView) adView.findViewById(R.id.native_ad_icon);
+                TextView nativeAdTitle = (TextView) adView.findViewById(R.id.native_ad_title);
+                MediaView nativeAdMedia = (MediaView) adView.findViewById(R.id.native_ad_media);
+                TextView nativeAdSocialContext = (TextView) adView.findViewById(R.id.native_ad_social_context);
+                TextView nativeAdBody = (TextView) adView.findViewById(R.id.native_ad_body);
+                Button nativeAdCallToAction = (Button) adView.findViewById(R.id.native_ad_call_to_action);
+
+                // Set the Text.
+                nativeAdTitle.setText(nativeAd.getAdTitle());
+                nativeAdSocialContext.setText(nativeAd.getAdSocialContext());
+                nativeAdBody.setText(nativeAd.getAdBody());
+                nativeAdCallToAction.setText(nativeAd.getAdCallToAction());
+
+                // Download and display the ad icon.
+                NativeAd.Image adIcon = nativeAd.getAdIcon();
+                NativeAd.downloadAndDisplayImage(adIcon, nativeAdIcon);
+
+                // Download and display the cover image.
+                nativeAdMedia.setNativeAd(nativeAd);
+
+                // Add the AdChoices icon
+                LinearLayout adChoicesContainer = (LinearLayout) findViewById(R.id.ad_choices_container);
+                AdChoicesView adChoicesView = new AdChoicesView(AdvertisementActivity.this, nativeAd, true);
+                adChoicesContainer.addView(adChoicesView);
+
+
+                // Register the Title and CTA button to listen for clicks.
+                List<View> clickableViews = new ArrayList<>();
+                clickableViews.add(nativeAdTitle);
+                clickableViews.add(nativeAdCallToAction);
+                nativeAd.registerViewForInteraction(nativeAdContainer,clickableViews);
+            }
+
+
+            @Override
+            public void onAdClicked(Ad ad) {
+                // Ad clicked callback
+            }
+
+            @Override
+            public void onLoggingImpression(Ad ad) {
+                // Ad impression logged callback
+            }
+        });
+
+        // Request an ad
+        nativeAd.loadAd();
+    }
+}
